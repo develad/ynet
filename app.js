f15a93ea3aea17c9c02cc2f409841c46a7541dc4
develad <develadb@gmail.com>
2024-05-08 11:16:37 +0300

    :ear_of_rice: Theme with radio update

--- app.js
+++ app.js
@@ -2,149 +2,24 @@ const card = document.querySelector('.news-card');
 const spinner = document.querySelector('.spinner');
 const logo = document.querySelector('.logo');
 const arrowUp = document.querySelector('.arrow-up');
-const _clockTime = document.querySelector('.clock-time');
-const _clockDate = document.querySelector('.clock-date');
-const _weather = document.querySelector('.weather');
-const _audio = new Audio('./alarm-sound.wav');
 
-const threats = {
-  0: {
-    category: 1,
-    code: 'missilealert',
-    duration: 10,
-    label: 'ירי רקטות וטילים',
-    description1: 'היכנסו למרחב המוגן ושהו בו 10 דקות',
-    description2: '',
-    link1: '',
-    link2: 'https://www.oref.org.il/12943-he/pakar.aspx',
-    matrixid: 1,
-  },
-  5: {
-    category: 2,
-    code: 'uav',
-    duration: 10,
-    label: 'חדירת כלי טיס עוין',
-    description1:
-      'היכנסו מיד למרחב המוגן ושהו בו למשך 10 דקות, אלא אם ניתנה התרעה נוספת',
-    description2: '',
-    link1: '',
-    link2: 'https://www.oref.org.il/12754-he/pakar.aspx',
-    matrixid: 6,
-  },
-};
+const _radio_btn = document.querySelector('.radio_btn');
+const _glgltz = document.querySelector('.glgltz');
+const body = document.querySelector('body');
 
-const RenderAlertsToScreen = (alertsToShow) => {
-  alertsToShow.reverse().map((item, index) => {
-    if (index === 0) {
-      _audio.play();
-    }
+const button = document.querySelector('#btn');
 
-    item.isDrill === false &&
-      Toastify({
-        text: `
-    <div style="display:flex;flex-direction: column;width:100%;padding: 0 0.5rem;line-height: 1.2;font-size:1.5rem;gap:0.5rem">
-        <div style="display:flex;justify-content: space-between;align-items: center;"><span style="">${moment(
-          new Date(item.time)
-        )
-          .locale('he')
-          .format('HH:mm')}${
-          threats[item.threat]?.label
-            ? ` - ${threats[item.threat].label}`
-            : ' - התראת צבע אדום'
-        }</span><img src='./PakarLogo.png'/></div>
-      <hr/>
-      <div style="flex:1;">${item.cities.join(' , ')}</div>
-    <hr/>
-    <div>${
-      threats[item.threat]?.description1
-        ? threats[item.threat].description1
-        : 'יש להיכנס לאתר פיקוד העורף ולפעול בהתאם להנחיות'
-    }</div>
-    </div>
-      `,
-        // Display the alert notification on the screen for 30 secondes
-        duration: 1000 * 30,
-        newWindow: true,
-        close: true,
-        escapeMarkup: false,
-        gravity: 'top', // `top` or `bottom`
-        position: 'right', // `left`, `center` or `right`
-        stopOnFocus: false, // Prevents dismissing of toast on hover
-        style: {
-          background: 'linear-gradient(to right, #cb2d3e, #ef473a)',
-        },
-      }).showToast();
-  });
-};
+button.addEventListener('change', (e) => {
+  body.classList.toggle('light-mode');
+});
 
-// const exampleData = [
-//   {
-//     notificationId: 'cfb22a75-8777-4195-a08f-139a91babb02',
-//     time: 1702206929,
-//     threat: 0,
-//     isDrill: false,
-//     cities: ['נתיב העשרה'],
-//   },
-//   {
-//     notificationId: 'ff268978-4737-47d6-b127-59e8d2db1549',
-//     time: 1702205108,
-//     threat: 5,
-//     isDrill: false,
-//     cities: ['נחל עוז'],
-//   },
-// ];
-
-// RenderAlertsToScreen(exampleData);
-
-let ids = [];
-let shownData = [];
-const getRedAlert = async () => {
-  const res = await fetch(
-    'https://dark-gray-snail-ring.cyclic.app/redAlertNotifications'
-  );
-  let data = await res.json();
-
-  data = data.map((item) => ({
-    ...item,
-    time: item.time * 1000,
-  }));
-
-  ids = shownData.map((item) => item.notificationId);
-
-  const fliterdData = data?.filter(
-    (item) => !ids.includes(item.notificationId)
-  );
-
-  shownData = [...shownData, ...fliterdData];
-  fliterdData.length !== 0 && RenderAlertsToScreen(fliterdData);
-};
-
-getRedAlert();
-setInterval(getRedAlert, 1000);
-
-const getWeather = async () => {
-  const res = await fetch(
-    'https://dark-gray-snail-ring.cyclic.app/minimal-forecast'
-  );
-  const data = await res.json();
-  _weather.innerHTML =
-    '<div style="text-align:center;margin-bottom:1.5rem;"><h1>תחזית מזג אוויר</h1><hr/></div>';
-  _weather.innerHTML += data
-    .map(
-      (item) =>
-        `<div>
-      <h2 style="margin-bottom:0.5rem; color:gold;">
-        ${item.daystring} | ${item.date.replaceAll('-', '.')}
-      </h2>
-      <p>${item.hebrewForecast}</p>
-    </div>
-    `
-    )
-    .join('<br/>');
-};
-
-getWeather();
-setInterval(getWeather, 1000 * 60 * 60);
+let isShowing = false;
+_radio_btn.addEventListener('click', (e) => {
+  isShowing = !isShowing;
+  isShowing
+    ? (_glgltz.style.display = 'block')
+    : (_glgltz.style.display = 'none');
+});
 
 const getData = async () => {
   spinner.style.display = 'block';
@@ -210,20 +85,3 @@ arrowUp.addEventListener('click', () => {
 
 getData();
 setInterval(getData, 1000 * 60 * 10);
-
-const clock = () => {
-  let timeString = moment().locale('he').format('LLLL');
-  timeString = timeString.split(' ');
-  _clockDate.innerHTML = `יום ${timeString[0]} ${timeString[1]} ${timeString[2]}`;
-  const [hour, minutes] = timeString[4].split(':');
-  if (minutes === '00') {
-    _clockTime.style.color = 'lime';
-    _clockTime.innerHTML = `${timeString[4]}`;
-  } else {
-    _clockTime.style.color = 'white';
-    _clockTime.innerHTML = `${timeString[4]}`;
-  }
-};
-
-clock();
-setInterval(clock, 1000);
